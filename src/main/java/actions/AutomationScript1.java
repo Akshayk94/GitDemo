dc082fc0abe77c9e8802b26f8a1260bbc0319236
kulkarniakshay82@gmail.com <kulkarniakshay82@gmail.com>
2024-04-23 09:41:07 +0530

    Commit to develop by Akshay

--- src/main/java/actions/AutomationScript1.java
+++ src/main/java/actions/AutomationScript1.java
@@ -8,10 +8,12 @@ import org.openqa.selenium.interactions.Actions;
 /*Move mouse pointer to specific element*/
 public class AutomationScript1 {
     public static void main(String[] args) {
+        System.out.println("Script Started");
         WebDriver driver=new FirefoxDriver();
         driver.get("https://www.amazon.in/ref=nav_logo");
 
         Actions a=new Actions(driver);
         a.moveToElement(driver.findElement(By.id("nav-link-accountList"))).perform();
+        System.out.println("Script Ended");
     }
 }
